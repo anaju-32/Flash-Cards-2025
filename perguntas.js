13119a7b57cdd8b0cfb2956666893a335118529b
anaju-32 <ana.miretzki@escola.pr.gov.br>
2025-06-13 10:24:57 -0300

    v2.1

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Artes',
+    'Qual é a cor do céu?',
+    'O céu é azul'
+)
+
+criaCartao(
+    'Matemátca',
+    'Quanto é 1+1?',
+    '1+1=2'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil foi descoberto?',
+    'O Brasil foi descoberto em 1500'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil',
+    'A capital do Brasil é Brasília'
+)
